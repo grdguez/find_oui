84caec54fe5bb12df4f0cbea71219dda36e96507
Heisenberg <36684964+grdguez@users.noreply.github.com>
2019-10-18 13:20:21 -0400

    Create find_mac.sh

---
+++ find_mac.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+wget http://standards-oui.ieee.org/oui/oui.txt
+
+MAC="$(echo $1 | sed 's/ //g' | sed 's/-//g' | sed 's/://g' | cut -c1-6)";
+  
+result="$(grep -i -A 4 ^$MAC ./oui.txt)";
+  
+if [ "$result" ]; then
+    echo "For the MAC $1 the following information is found:"
+    echo "$result"
+else
+    echo "MAC $1 is not found in the database."
+fi
